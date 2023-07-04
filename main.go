c6edf7319f8902b1d4bf6c79585f75eaae39efe9
RaifZeidam <102070988+RaifZeidam@users.noreply.github.com>
2023-07-03 22:33:04 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,53 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/v2Kamikaze/SGBD-2/src"
 	"github.com/v2Kamikaze/SGBD-2/src/scheduler"
 )
 
 func main() {
-	//q := "BT(1)r1(x)BT(2)w2(x)r2(y)r1(y)C(1)r2(z)C(2)"
-	q := "BT(1)r1(x)BT(2)w2(y)r1(y)w2(x)C(1)r2(z)C(2)"
+	fmt.Println("Escolha o nível de isolamento:")
+	fmt.Println("1 - Read Uncommitted")
+	fmt.Println("2 - Read Committed")
+	fmt.Println("3 - Repeatable Read")
+	fmt.Println("4 - Serializable")
 
-	scheduling := src.ParseOperations(q)
+	// var isolationLevel scheduler.IsolationLevel
+
+	// for {
+	// 	fmt.Print("Digite o número correspondente ao nível de isolamento: ")
+	// 	var input string
+	// 	fmt.Scanln(&input)
+	// 	input = strings.TrimSpace(input)
+	// 	if input == "1" {
+	// 		isolationLevel = scheduler.ReadUncommitted
+	// 		break
+	// 	} else if input == "2" {
+	// 		isolationLevel = scheduler.ReadCommitted
+	// 		break
+	// 	} else if input == "3" {
+	// 		isolationLevel = scheduler.RepeatableRead
+	// 		break
+	// 	} else if input == "4" {
+	// 		isolationLevel = scheduler.Serializable
+	// 		break
+	// 	} else {
+	// 		fmt.Println("Opção inválida. Digite novamente.")
+	// 	}
+	// }
+
+	fmt.Print("Digite a sequência de operações de escalonamento (exemplo: BT(1)r1(x)BT(2)w2(x)r2(y)r1(y)C(1)r2(z)C(2)): ")
+	var transactionString string
+	fmt.Scanln(&transactionString)
+
+	scheduling := src.ParseOperations(transactionString)
+
 	scheduler := scheduler.New(scheduling)
+	// scheduler.SetIsolationLevel(isolationLevel)
+
+	fmt.Println("Executando escalonamento...")
 	scheduler.Start()
 }
 
